2b7b540531da847568bcdfd8629a8918e1e00a9b
Baraah <alsadibaraah@gmail.com>
2026-06-23 09:58:01 +0400

    added case 1

--- FlightManagementSystem/Program.cs
+++ FlightManagementSystem/Program.cs
@@ -1,6 +1,6 @@
 ﻿using FlightManagementSystem.Models;
 using System;
-
+using System.Linq;
 namespace FlightManagementSystem
 {
     internal class Program
@@ -9,11 +9,57 @@ namespace FlightManagementSystem
         // It stores all passengers, pilots, aircrafts, flights, and bookings.
         public static FlightContext context = new FlightContext();
 
-        //
 
- 
+        // ---------------------------------------------------------
+        // case 1
+        public static void RegisterPassenger()
+        {
+            Console.WriteLine("---- Register Passenger ----");
 
-        
+            Console.Write("Enter passenger name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter passenger email: ");
+            string email = Console.ReadLine();
+
+            Console.Write("Enter passenger phone: ");
+            string phone = Console.ReadLine();
+
+            Console.Write("Enter passport number: ");
+            string passport = Console.ReadLine();
+
+            // Passport number must be unique.
+            //linq
+            bool passportExists = context.Passengers.Any(p => p.passportNumber == passport);
+
+            if (passportExists)
+            {
+                Console.WriteLine("This passport already exists.");
+                return;
+            }
+
+            Console.Write("Enter nationality: ");
+            string nationality = Console.ReadLine();
+
+            Passenger passenger = new Passenger
+            {
+                passengerId = context.Passengers.Count + 1,
+                passengerName = name,
+                passengerEmail = email,
+                passengerPhone = phone,
+                passportNumber = passport,
+                nationality = nationality
+            };
+
+            context.Passengers.Add(passenger);
+
+            Console.WriteLine("Passenger registered successfully.");
+            Console.WriteLine("Passenger ID: " + passenger.passengerId);
+        }
+
+
+      
+
         // ---------------------------------------------------------
         // Main Menu
         // 
@@ -52,6 +98,7 @@ namespace FlightManagementSystem
                 switch (choice)
                 {
                     case 1:
+                        RegisterPassenger();
                         break;
 
                     case 2:
