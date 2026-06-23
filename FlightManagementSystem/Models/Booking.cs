e2f336ddcf6965cb3286173f65fb2638a25e0ed3
Baraah <alsadibaraah@gmail.com>
2026-06-23 08:28:49 +0400

    added booking properties

--- FlightManagementSystem/Models/Booking.cs
+++ FlightManagementSystem/Models/Booking.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FlightManagementSystem.Models
+﻿namespace FlightManagementSystem.Models
 {
-    internal class Booking
+    public class Booking
     {
+        public int bookingId { get; set; }
+        public int passengerId { get; set; }
+        public int flightId { get; set; }
+        public string seatNumber { get; set; }
+        public string bookingDate { get; set; }
+        public decimal totalPrice { get; set; }
+        public string status { get; set; }
     }
 }
