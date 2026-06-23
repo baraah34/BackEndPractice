0b932913435051185d02181cd1a05a2128090231
Baraah <alsadibaraah@gmail.com>
2026-06-23 08:29:27 +0400

    added all list in flight context

--- FlightManagementSystem/FlightContext.cs
+++ FlightManagementSystem/FlightContext.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using FlightManagementSystem.Models;
 using System.Collections.Generic;
-using System.Text;
 
 namespace FlightManagementSystem
 {
-    internal class FlightContext
+    public class FlightContext
     {
+        public List<Passenger> Passengers { get; set; } = new List<Passenger>();
+        public List<Pilot> Pilots { get; set; } = new List<Pilot>();
+        public List<Aircraft> Aircrafts { get; set; } = new List<Aircraft>();
+        public List<Flight> Flights { get; set; } = new List<Flight>();
+        public List<Booking> Bookings { get; set; } = new List<Booking>();
     }
 }
