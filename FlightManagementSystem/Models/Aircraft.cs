1fb2c1ea0391dd849a53ce8cf5294c76390fcba4
Baraah <alsadibaraah@gmail.com>
2026-06-23 08:25:31 +0400

    added aircraft Properties

--- FlightManagementSystem/Models/Aircraft.cs
+++ FlightManagementSystem/Models/Aircraft.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace FlightManagementSystem.Models
+﻿namespace FlightManagementSystem.Models
 {
-    internal class Aircraft
+    public class Aircraft
     {
+        public int aircraftId { get; set; }
+        public string model { get; set; }
+        public int totalSeats { get; set; }
+        public bool isOperational { get; set; }
     }
 }
