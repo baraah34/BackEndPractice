4dcbf331f16ee71734ef8a9f64a2dcf8256a22d1
Baraah <alsadibaraah@gmail.com>
2026-06-30 16:13:58 +0400

    added attributes for Enrollment class

--- UniversitySystem/Models/Enrollment.cs
+++ UniversitySystem/Models/Enrollment.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace UniversitySystem.Models
 {
-    internal class Enrollment
+    public class Enrollment
     {
+      
+        public int enrollmentId { get; set; } 
+
+        public int studentId { get; set; } 
+
+        public int courseId { get; set; } 
+
+       
+        public DateTime enrollmentDate { get; set; }
+
+       
+        public string finalGrade { get; set; } 
+
+        public string status { get; set; }
     }
 }
