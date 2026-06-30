6dcf998a5c42d6437f63b98b437c9868545cbd8a
Baraah <alsadibaraah@gmail.com>
2026-06-30 16:11:02 +0400

    added attributes for the department class

--- UniversitySystem/Models/Department.cs
+++ UniversitySystem/Models/Department.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace UniversitySystem.Models
 {
-    internal class Department
+    public class Department
     {
+        public int departmentId { get; set; } 
+
+      
+        public string departmentName { get; set; } 
+
+        public string building { get; set; } 
+
+        
+        public decimal budget { get; set; } 
+
+        
+        public int? headInstructorId { get; set; } 
     }
 }
