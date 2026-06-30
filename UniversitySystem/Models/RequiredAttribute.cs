5122c27b9e1daf2853ab5189d2b77aa7d319479c
Baraah <alsadibaraah@gmail.com>
2026-06-30 16:01:50 +0400

    added attribute of student class

---
+++ UniversitySystem/Models/RequiredAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace UniversityERDModels.Models
+{
+    internal class RequiredAttribute : Attribute
+    {
+    }
+}
--- UniversitySystem/Models/Student.cs
+++ UniversitySystem/Models/Student.cs
@@ -1,12 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace UniversitySystem.Models
 {
-    internal class Student
+    public class Student
     {
+   
+        public int studentId { get; set; } 
+
+     
+        public string fullName { get; set; } 
+
+    
+        public string email { get; set; } 
+
+      
+        public string phoneNumber { get; set; } 
+
+    
+        public DateTime dateOfBirth { get; set; } 
+        
+        public int enrollmentYear { get; set; }
+
+   
+        public decimal gpa { get; set; }
     }
 }
