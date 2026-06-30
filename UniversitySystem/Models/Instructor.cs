15e7cbae4419e6817698d07153b42d8f764fe59a
Baraah <alsadibaraah@gmail.com>
2026-06-30 16:09:43 +0400

    added attributes for instructor class

--- UniversitySystem/Models/Instructor.cs
+++ UniversitySystem/Models/Instructor.cs
@@ -1,12 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UniversitySystem.Models
 {
-    internal class Instructor
+    public class Instructor
     {
+        
+        public int instructorId { get; set; } 
+
+      
+        public string fullName { get; set; } 
+
+      
+        public string email { get; set; } 
+
+        public string officeNumber { get; set; } 
+
+        public DateTime hireDate { get; set; } 
+
+        public decimal salary { get; set; } 
+
+        public string academicTitle { get; set; } 
     }
 }
