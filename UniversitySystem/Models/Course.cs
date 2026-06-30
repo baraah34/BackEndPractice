6dd91e98e71889c322de4b1644690cd26b1fa8e5
Baraah <alsadibaraah@gmail.com>
2026-06-30 16:12:48 +0400

    added attributes for course class

--- UniversitySystem/Models/Course.cs
+++ UniversitySystem/Models/Course.cs
@@ -1,12 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace UniversitySystem.Models
 {
-    internal class Course
+    public class Course
     {
+      
+        public int courseId { get; set; } 
+
+      
+        public string courseCode { get; set; } 
+
+      
+        public string courseTitle { get; set; } 
+
+   
+        public int creditHours { get; set; }
+
+    
+        public int departmentId { get; set; } 
+
+     
+        public int? instructorId { get; set; } 
+
+        public string semesterOffered { get; set; } 
     }
 }
