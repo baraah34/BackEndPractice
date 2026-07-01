807af75f55018126cfe7209e555dea10d871d50a
Baraah <alsadibaraah@gmail.com>
2026-07-01 10:31:14 +0400

    added data annotations for student class

--- UniversitySystem/Models/Student.cs
+++ UniversitySystem/Models/Student.cs
@@ -1,28 +1,35 @@
 ﻿using System;
-
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace UniversitySystem.Models
 {
     public class Student
     {
-   
-        public int studentId { get; set; } 
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int studentId { get; set; } // system generated
 
-     
-        public string fullName { get; set; } 
+        [Required]
+        [MaxLength(100)]
+        public string fullName { get; set; } = string.Empty; // user input
 
-    
-        public string email { get; set; } 
+        [Required]
+        [MaxLength(150)]
+        public string email { get; set; } = string.Empty; // user input, unique
 
-      
-        public string phoneNumber { get; set; } 
+        [MaxLength(20)]
+        public string phoneNumber { get; set; } // user input, optional
 
-    
-        public DateTime dateOfBirth { get; set; } 
-        
-        public int enrollmentYear { get; set; }
+        [Required]
+        public DateTime dateOfBirth { get; set; } // user input
 
-   
-        public decimal gpa { get; set; }
+        [Required]
+        [Range(2000, 2030)]//years 
+        public int enrollmentYear { get; set; } // user input
+
+        [Column(TypeName = "decimal(3,2)")]//how will store gpa in database 
+        [Range(typeof(decimal), "0.0", "4.0")]//gpa must be between 0.0 and 4.0
+        public decimal gpa { get; set; } = 0.0m; // default value
     }
 }
