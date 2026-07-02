2db8115593f26160e15034d3cb4784495b33c7f7
Baraah <alsadibaraah@gmail.com>
2026-07-02 09:06:59 +0400

    added attributes for OrderProduct class

--- ECommerce_System/Models/OrderProduct.cs
+++ ECommerce_System/Models/OrderProduct.cs
@@ -1,10 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
-namespace ECommerce_System.Models
+namespace ECommerceSystem.Models
 {
-    internal class OrderProduct
+    public class OrderProduct
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int orderProductId { get; set; } // system generated
+
+        [Required]
+        [ForeignKey("Order")]
+        public int orderId { get; set; } // foreign key
+
+        [Required]
+        [ForeignKey("Product")]
+        public int productId { get; set; } // foreign key
+
+        [Required]
+        [Range(1, 999)]
+        public int quantity { get; set; } // user input
+
+       
     }
 }
