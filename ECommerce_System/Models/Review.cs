ba9ef4085381130df24fd86f9ced6fca337a3bb9
Baraah <alsadibaraah@gmail.com>
2026-07-02 08:41:25 +0400

    added relations for review class

--- ECommerce_System/Models/Review.cs
+++ ECommerce_System/Models/Review.cs
@@ -30,5 +30,9 @@ namespace ECommerceSystem.Models
         [Required]
         public DateTime reviewDate { get; set; } // system generated
 
+        public User? User { get; set; } // relationship ==> many reviews are written by one user
+
+        public Product? Product { get; set; } // relationship ==> many reviews belong to one product
+
     }
 }
