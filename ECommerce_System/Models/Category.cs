feb654cf768e9329c76bcfdf623b28af6143f391
Baraah <alsadibaraah@gmail.com>
2026-07-02 08:10:08 +0400

    added attributes for category class

--- ECommerce_System/Models/Category.cs
+++ ECommerce_System/Models/Category.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using ECommerce_System.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
-namespace ECommerce_System.Models
+namespace ECommerceSystem.Models
 {
-    internal class Category
+    public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int categoryId { get; set; } // system generated
+
+        [Required]
+        [MaxLength(100)]
+        public string categoryName { get; set; } = string.Empty; // user input, unique
+
+        [MaxLength(500)]
+        public string? description { get; set; } // user input, optional
+
+        [MaxLength(300)]
+        public string? imageUrl { get; set; } // user input, optional
+
     }
 }
