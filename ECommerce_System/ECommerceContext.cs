14507b2c15af2118c8c9378a2c096c5bfebd1f33
Baraah <alsadibaraah@gmail.com>
2026-07-06 14:54:46 +0400

    Upgrade ECommerceContext to Inherit from DbContext

---
+++ ECommerce_System/ECommerceContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ECommerceSystem.Models;
+
+namespace ECommerce_System
+{
+    internal class ECommerceContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+
+        // Bridge table between Order and Product
+        public DbSet<OrderProduct> OrderProducts { get; set; }
+
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
