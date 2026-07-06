b9d4a23c7e35fbaec22256f82309a740df549ac4
Baraah <alsadibaraah@gmail.com>
2026-07-06 15:36:55 +0400

    added main menu

--- ECommerce_System/Program.cs
+++ ECommerce_System/Program.cs
@@ -4,7 +4,85 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int choice;
+
+            do
+            {
+                Console.WriteLine("\n----- E-Commerce EF Core System ----");
+                Console.WriteLine("1. Register New User");
+                Console.WriteLine("2. Add New Product");
+                Console.WriteLine("3. Place Order");
+                Console.WriteLine("4. Write Product Review");
+                Console.WriteLine("5. Update Product Price and Availability");
+                Console.WriteLine("6. Cancel Order");
+                Console.WriteLine("7. Delete Review");
+                Console.WriteLine("8. View All Products");
+                Console.WriteLine("9. Filter Products by Category and Price Range");
+                Console.WriteLine("10. Get Category With Products");
+                Console.WriteLine("11. View User Order History");
+                Console.WriteLine("12. Product Summary Report");
+                Console.WriteLine("0. Exit");
+
+                Console.Write("Enter choice: ");
+                int.TryParse(Console.ReadLine(), out choice);
+
+                switch (choice)
+                {
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                    case 4:
+                        break;
+
+                    case 5:
+                        break;
+
+                    case 6:
+                        break;
+
+                    case 7:
+                        break;
+
+                    case 8:
+                        break;
+
+                    case 9:
+                        break;
+
+                    case 10:
+                        break;
+
+                    case 11:
+                      
+                        break;
+
+                    case 12:
+                        
+                        break;
+
+                    case 0:
+                        Console.WriteLine("Goodbye!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice.");
+                        break;
+                }
+
+                if (choice != 0)
+                {
+                    Console.WriteLine("\nPress any key to continue...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+
+            } while (choice != 0);
         }
     }
 }
